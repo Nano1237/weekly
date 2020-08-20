edbb78f75f6afc8a0c8e9dcbe9a1b7a905edd4c6
Tim Rücker <tim.ruecker@ymail.com>
2020-08-20 18:30:42 +0200

    editing the item list

--- src/items.js
+++ src/items.js
@@ -2,86 +2,95 @@
     const weekNow = weeksBetween(new Date('2020-08-25'), new Date());
 
     const items = [
-        // 35
-        'Nach Polen fahren und dort mit einem VW Caddy Maxi Campen 🇵🇱',
-        // 36
-        '<a href="http://aldar.de/hannover/" target="_blank">In einem syrischen Restaurant essen gehen 🧆</a>',
-        // 37
-        'Eine Sache machen, die „nur ein Kind“ machen würde (z. B. Pfützenspringen 🌧️, Scheibe Wurst an der Theke erfragen 🥓 oder mit Murmeln spielen 🔴)',
-        // 38
-        'Einen Handabdruck in feuchtem Zement hinterlassen 🖐️',
-        // 39
-        '<a href="https://www.tv-plus.de/bingo-tickets/" target="_blank" title="Weil wir solche Rentner sind">Bingo spielen und laut Bingo rufen 🎲</a>',
-        // 40
-        'Ein Instrument lernen 🎸',
-        // 41
-        'Einen Telefonstreich machen 📞',
-        // 42
-        'Ein Lied im Radio wünschen 📻',
-        // 43
-        'Ein 5000-Teile Puzzle schaffen 🧩',
-        // 44
-        'Ein neues Buch anfangen 📖',
-        // 45
-        'An einem Ort in deiner Stadt Müll aufräumen 🛢️',
-        // 46
-        'Etwas gemeinnütziges tun 🍲',
-        // 47
-        'Insekten essen 🐛',
-        // 48
-        'Einen Standard-Tanzkurs besuchen 💃🕺',
-        // 49
-        'Ein Bild fürs eigene Zuhause malen 🖼️',
-        // 50
-        'Bei einem Dinner im Dunkeln mitmachen ◼️',
-        // 51
-        'Deinen Geburtstag richtig feiern! 🎁',
-        // 52
-        'eine Schlittenfahrt im Schnee 🛷',
-        // 53
-        'In einer heißen Quelle baden ♨️',
-        // KW 1 kommt jetzt !!
-        'Die Polarlichter sehen 🦄',
-        // 2
-        'Einen Töpferkurs besuchen 🍯',
-        // 3
-        'Eine Kerze selbermachen 🕯️',
-        // 4
-        'Ein Lied in einer Karaoke-Bar singen 🎤',
-        // 5
-        'In einen Indoor Trampolin-Park gehen 🎫',
-        // 6
-        'Einem Escape-Room entkommen 🗝️',
-        // 7
-        'Bei einer Pub-Quiznacht mitmachen 🍺️',
-        // 8
-        'Bouldern gehen 🧗',
-        // 9
-        'Auf einem Kamel reiten 🐫',
-        // 10
-        'Auf einer Vespa durch Italien fahren 🛵',
-        // 11
-        'Zorbing 🎱',
-        // 12
-        'Eine Zeitkapsel vergraben ⏳',
-        // 13
-        'Zu einen Schiffswrack tauchen 🚢',
-        // 14
-        '',
-        // 15
-        '',
-        // 16
-        '',
-        // 17
-        '',
-        // 18
-        'Einen Vulkan besteigen 🌋',
-        // 19
-        'Auf einen Eisberg klettern 🧊',
+        // 35},
+        {text: 'Nach Polen fahren und dort mit einem VW Caddy Maxi Campen', emoji: '🇵🇱'},
+        // 36},
+        {
+            text: '<a href="http://aldar.de/hannover/" target="_blank">In einem syrischen Restaurant essen gehen</a>',
+            emoji: '🧆'
+        },
+        // 37},
+        {
+            text: 'Eine Sache machen, die „nur ein Kind“ machen würde (z. B. Pfützenspringen, Scheibe Wurst an der Theke erfragen oder mit Murmeln spielen)',
+            emoji: '🧒'
+        },
+        // 38},
+        {text: 'Einen Handabdruck in feuchtem Zement hinterlassen️', emoji: '🖐'},
+        // 39},
+        {
+            text: '<a href="https://www.tv-plus.de/bingo-tickets/" target="_blank" title="Weil wir solche Rentner sind">Bingo spielen und laut Bingo rufen</a>',
+            emoji: '🎲'
+        },
+        // 40},
+        {text: 'Ein Instrument lernen', emoji: '🎸'},
+        // 41},
+        {text: 'Einen Telefonstreich machen', emoji: '📞'},
+        // 42},
+        {text: 'Ein Lied im Radio wünschen', emoji: '📻'},
+        // 43},
+        {text: 'Ein 5000-Teile Puzzle schaffen', emoji: '🧩'},
+        // 44},
+        {text: 'Ein neues Buch anfangen', emoji: '📖'},
+        // 45},
+        {text: 'An einem Ort in deiner Stadt Müll aufräumen', emoji: '🛢️'},
+        // 46},
+        {text: 'Etwas gemeinnütziges tun', emoji: '🍲'},
+        // 47},
+        {text: 'Insekten essen', emoji: '🐛'},
+        // 48},
+        {text: 'Einen Standard-Tanzkurs besuchen', emoji: '💃🕺'},
+        // 49},
+        {text: 'Ein Bild fürs eigene Zuhause malen', emoji: '🖼️'},
+        // 50},
+        {text: 'Bei einem Dinner im Dunkeln mitmachen', emoji: '◼️'},
+        // 51},
+        {text: 'Deinen Geburtstag richtig feiern!', emoji: '🎁'},
+        // 52},
+        {text: 'eine Schlittenfahrt im Schnee', emoji: '🛷'},
+        // 53},
+        {text: 'In einer heißen Quelle baden', emoji: '♨️'},
+        // KW 1 kommt jetzt !!},
+        {text: 'Die Polarlichter sehen', emoji: '🦄'},
+        // 2},
+        {text: 'Einen Töpferkurs besuchen', emoji: '🍯'},
+        // 3},
+        {text: 'Eine Kerze selbermachen', emoji: '🕯️'},
+        // 4},
+        {text: 'Ein Lied in einer Karaoke-Bar singen', emoji: '🎤'},
+        // 5},
+        {text: 'In einen Indoor Trampolin-Park gehen', emoji: '🎫'},
+        // 6},
+        {text: 'Einem Escape-Room entkommen', emoji: '🗝️'},
+        // 7},
+        {text: 'Bei einer Pub-Quiznacht mitmachen', emoji: '🍺️'},
+        // 8},
+        {text: 'Bouldern gehen', emoji: '🧗'},
+        // 9},
+        {text: 'Auf einem Kamel reiten', emoji: '🐫'},
+        // 10},
+        {text: 'Auf einer Vespa durch Italien fahren', emoji: '🛵'},
+        // 11},
+        {text: 'Zorbing', emoji: '🎱'},
+        // 12},
+        {text: 'Eine Zeitkapsel vergraben', emoji: '⏳'},
+        // 13},
+        {text: 'Zu einen Schiffswrack tauchen', emoji: '🚢'},
+        // 14},
+        {text: '', emoji: ''},
+        // 15},
+        {text: '', emoji: ''},
+        // 16},
+        {text: '', emoji: ''},
+        // 17},
+        {text: '', emoji: ''},
+        // 18},
+        {text: 'Einen Vulkan besteigen', emoji: '🌋'},
+        // 19},
+        {text: 'Auf einen Eisberg klettern', emoji: '🧊'},
     ];
-
-    document.querySelector('[data-kw]').innerHTML = `${weekNow <= 0 ? 1 : weekNow + 1}. Woche`;
-    document.querySelector('[data-text]').innerHTML = getCurrentElement();
+    const element = getCurrentElement();
+    document.querySelector('[data-kw]').innerHTML = `${weekNow <= 0 ? 1 : weekNow + 1}. Woche ${element.emoji}`;
+    document.querySelector('[data-text]').innerHTML = element.text;
 
     /**
      * @description
